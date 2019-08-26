2b5dc25807699ffe0043659897530b9c9aab6e93
mmarszal7 <mmarszal7@gmail.com>
2019-08-26 16:10:32 +0200

    Add ability to save formatted JSONs into files (useful for big JSONs)

--- apps/json-formatter/formatter.py
+++ apps/json-formatter/formatter.py
@@ -32,11 +32,29 @@ def formatter(json, indent):
 	
 	return newJson
 
+def getJSONfromFile():
+	print("Enter file name:")
+	filename = input()
+	f = open(filename, "r", encoding="utf8")
+	json = f.read()
+	f.close()
+	return json
+	
+def getJSONfromConsole():
+	print('Paste json:')
+	return input()
+	
+def saveJSONtoFile(json):
+	f = open("temp.json", "w", encoding="utf8")
+	f.write(json)
+	f.close()
+
 print('Paste indent:')
 indent = input()
-print('Paste json:')
-json = input()
+print("Press 1 if you want to convert words from a file:")
+json = getJSONfromFile() if input() == "1" else getJSONfromConsole()
 
 formattedJSON = formatter(json, indent)
 toClipboard(formattedJSON)
-print('\n' + formattedJSON)
+saveJSONtoFile(formattedJSON)
+print('\n' + formattedJSON + '\nJSON copied to clipboard and save in temp.json file!')
